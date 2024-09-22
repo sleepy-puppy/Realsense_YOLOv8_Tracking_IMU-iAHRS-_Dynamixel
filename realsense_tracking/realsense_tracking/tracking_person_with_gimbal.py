bc81e31ce9ca7f94159c293b0471d3eefb7b65d5
sleepy-puppy <142367469+sleepy-puppy@users.noreply.github.com>
2024-09-22 21:13:29 +0900

    Update tracking_person_with_gimbal.py

--- realsense_tracking/realsense_tracking/tracking_person_with_gimbal.py
+++ realsense_tracking/realsense_tracking/tracking_person_with_gimbal.py
@@ -12,40 +12,41 @@ class DepthSubscriber(Node):
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         self.robot_v = 0.0  # 초기화
         self.wheelbase = 0.84  # 로봇의 휠베이스(L)를 가정 (단위: meter)
-        self.current_yaw = 0
+        self.current_yaw = None
         
     def target_person_depth_listener_callback(self, msg):
         person_id = int(msg.data[0])
         depth = msg.data[1]
 
-        # if depth > 0.5:
-        #     print(f"ID {person_id}: 깊이 값이 0.5m보다 큼 ({depth:.2f} m)")
-        
         # 깊이에 따른 속도 계산
         self.robot_v = self.calculate_robot_v(depth)
 
-        # yaw 값을 받아서 오메가 계산
-        robot_w = self.calculate_robot_w(self.current_yaw)
+        if self.current_yaw is not None and depth is not None:
+            # yaw 값을 받아서 오메가 계산
+            robot_w = self.calculate_robot_w(self.current_yaw)
 
-        print(f"ID {person_id}: robot_w = {robot_w:.2f}, robot_v = {self.robot_v:.2f}")
+            print(f"ID {person_id}: robot_w = {robot_w:.2f}, robot_v = {self.robot_v:.2f}")
 
-        # Twist 메시지 발행
-        twist_msg = Twist()
-        twist_msg.linear = Vector3(x=self.robot_v, y=0.0, z=0.0)
-        twist_msg.angular = Vector3(x=0.0, y=0.0, z=robot_w)
-        self.publisher_.publish(twist_msg)
+            # Twist 메시지 발행
+            twist_msg = Twist()
+            twist_msg.linear = Vector3(x=self.robot_v, y=0.0, z=0.0)
+            twist_msg.angular = Vector3(x=0.0, y=0.0, z=robot_w)
+            self.publisher_.publish(twist_msg)
+        
+        else:
+            print(f"ID {person_id}: yaw 또는 depth 값이 유효하지 않음 (current_yaw={self.current_yaw}, depth={depth})")
+
         
     def yaw_callback(self, msg):
-        self.current_yaw = msg.data
+        self.current_yaw = msg.data if msg.data is not None else None
     
     def calculate_robot_w(self, yaw):
         # yaw 값을 통해 조향각 계산 (단위: degree)
-        print(f"dynamixel yaw : {self.current_yaw}")
         steering_angle = 360 * (yaw - 2048) / 4096
         
         # 조향각을 -180도에서 180도로 제한
         steering_angle = max(min(steering_angle, 180), -180)
-        print(f"steering angle : {steering_angle}")
+        print(steering_angle)
 
         if abs(steering_angle) <= 5:
             return 0.0
@@ -60,20 +61,18 @@ class DepthSubscriber(Node):
             robot_w = max_w * (steering_angle / 180.0)
             
             # 각속도는 로봇의 속도에 비례하게 조정
-            robot_w = robot_w * (self.robot_v / 0.2)
+            robot_w = robot_w * (self.robot_v / 0.5)
         
         return robot_w
 
     def calculate_robot_v(self, depth):
         # depth가 0.5m 이상 4m 이하일 때, 이를 0부터 1.0까지 선형 변환
-        min_depth = 1.5
+        min_depth = 0.5
         max_depth = 4.0
         if depth < min_depth:
             return 0.0
-        
         normalized_depth = min(max(depth - min_depth, 0), max_depth - min_depth)
-        robot_v = (normalized_depth / (max_depth - min_depth)) * 0.2
-        print(f"depth : {depth}")
+        robot_v = (normalized_depth / (max_depth - min_depth)) * 0.5
         return robot_v
 
 
